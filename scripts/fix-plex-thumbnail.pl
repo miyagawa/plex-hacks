077523e6d1391deab6038905b6aa415b2b808424
Tatsuhiko Miyagawa <miyagawa@bulknews.net>
2012-12-28 18:25:08 -0800

    added comment

--- scripts/fix-plex-thumbnail.pl
+++ scripts/fix-plex-thumbnail.pl
@@ -1,5 +1,15 @@
 #!/usr/bin/env perl
 # Script to swap EyeTV generated MP4 thumbnail with Plex generated one
+#
+# EyeTV generates its own thumbnail embedded in exported MP4 files for
+# iTunes Home Sharing, which is great, but there are several issues:
+# a) thumbnails have the title and EyeTV logo embedded and b) screen
+# capture is from 0s, which usually shows the commercials from the
+# previous show.
+#
+# This script tries to address that by replacing the thumbnail with
+# Plex's own thumbnail by using Plex's HTTP API. You need perl 5.8 or
+# later with LWP module installed.
 
 use strict;
 use URI;
